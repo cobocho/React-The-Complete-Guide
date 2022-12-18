5f9f0fe908d71dcddb7fea550790d5cc0456e15b
Cobocho <99083803+Cobocho@users.noreply.github.com>
2022-12-19 00:50:35 +0900

    Section12 Completed

--- Section12/src/App.js
+++ Section12/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const clickHandler = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {isClicked && <p>This is new!</p>}
+      <Button type="click" onClick={clickHandler}>
+        Click!
+      </Button>
     </div>
   );
 }
